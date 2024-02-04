51d2ec0fb14295cd8915f297a92b834d50e0b423
darwannneee <darwanchannel1@gmail.com>
2024-02-04 22:08:44 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import fetch from 'node-fetch';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/api/sales', async (req, res) => {
+    try {
+        const response = await fetch('https://api.prod.pallet.exchange/api/v1/marketplace/activities?chain_id=pacific-1&nft_address=sei1za9z9l8pwueataj5gx7mwt8g5zrndhjls7yr6tnhan0zlgw2r8fszn5089&page=1&page_size=500000');
+        const result = await response.json();
+        const data = result.activities;
+
+        const block = data.filter(seiyans => seiyans.block > 55647474);
+        const sale = block.filter(aa => aa.event_type === "sale");
+
+        const buyerCounts = sale.reduce((acc, item) => {
+            const buyer = item.buyer || 'Unknown Buyer';
+            acc[buyer] = (acc[buyer] || 0) + 1;
+            return acc;
+        }, {});
+
+        const sortedBuyerCounts = Object.entries(buyerCounts)
+            .sort((a, b) => b[1] - a[1])
+            .reduce((acc, [key, value]) => {
+                acc[key] = value;
+                return acc;
+            }, {});
+
+        const responseData = {
+            totalSales: sale.length,
+            buyerCounts: sortedBuyerCounts
+        };
+
+        res.json(responseData);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
