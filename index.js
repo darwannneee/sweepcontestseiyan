32b1a181da9a305e3ecc4704b37898edc3a4807e
darwannneee <darwanchannel1@gmail.com>
2024-02-06 03:21:22 +0700

    added new

--- index.js
+++ index.js
@@ -38,6 +38,30 @@ app.get('/api/sales', async (req, res) => {
     }
 });
 
+
+app.get('/api/address', async (req, res) => {
+    try {
+        const response = await fetch('https://api.prod.pallet.exchange/api/v1/marketplace/activities?chain_id=pacific-1&event_type=sale&nft_address=sei1za9z9l8pwueataj5gx7mwt8g5zrndhjls7yr6tnhan0zlgw2r8fszn5089&page=1&page_size=500');
+        const result = await response.json();
+        const data = result.activities;
+
+        // Filter and extract specific fields
+        const filteredData = data
+            .filter(seiyans => seiyans.block > 55647474 && seiyans.event_type === "sale")
+            .map(({ buyer, price_value, ts, token }) => ({
+                buyer,
+                price_value,
+                ts,
+                token_name: token.name
+            }));
+
+        res.json(filteredData);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
